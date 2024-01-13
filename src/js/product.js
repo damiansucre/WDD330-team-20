0db73cc058041d71d3e0ba6a272a6bd1d2cf5c20
damiansucre <damian_escalante_83@hotmail.com>
2024-01-13 16:47:01 -0300

    Fix add item to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,13 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs"; //I add the getLocalStorage
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
+const cartItems = getLocalStorage("so-cart") || []; //Get products currently in cart, if empty initilize an array
+
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  cartItems.push(product); //Adding products to the cart
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
