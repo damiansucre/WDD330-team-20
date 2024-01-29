1d4b6649fb90ed6bf48a724a365e37ea114effb0
damiansucre <damian_escalante_83@hotmail.com>
2024-01-28 22:31:52 -0300

    Team_activity_week_3

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,3 @@
-import ProductData from "./ProductData.mjs";
-import ProductListing from "./ProductList.mjs";
-//new code
 import { loadHeaderFooter } from "./utils.mjs";
 
-const dataSource = new ProductData("tents");
-const list = new ProductListing(
-  "Tents",
-  dataSource,
-  document.querySelector(".product-list")
-);
-
-list.init();
-//new code
 loadHeaderFooter();
