7369222300d8ce66f3e0f5ca19ef3c0fd45a8a5a
damiansucre <damian_escalante_83@hotmail.com>
2024-02-01 01:42:20 -0300

    week4-team-activity

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,20 +1,22 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
-/* function productDetailsTemplate(product) {
-  const template = `<section class="product-detail"> 
-  <h3>${product.Brand.Name}</h3>
+function productDetailsTemplate(product) {
+  return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
     <h2 class="divider">${product.NameWithoutBrand}</h2>
-
-    <img class="divider" src="${product.Image}" alt="${product.NameWithoutBrand}" />
-    
-    <p class="product-card__price"><span class="product-card__discount">$${product.SuggestedRetailPrice}</span>$${product.FinalPrice} span class="flag-discount">sale </span</p>
+    <img
+      class="divider"
+      src="${product.Images.PrimaryLarge}"
+      alt="${product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${product.FinalPrice}</p>
     <p class="product__color">${product.Colors[0].ColorName}</p>
-    <p class="product__description">${product.DescriptionHtmlSimple}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
     <div class="product-detail__add">
       <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-    </div>
-  </section>`;
-} */
+    </div></section>`;
+}
 
 export default class ProductDetails {
   constructor(productId, dataSource) {
@@ -28,54 +30,26 @@ export default class ProductDetails {
     // once we have the product details we can render out the HTML
     this.renderProductDetails("main");
     // once the HTML is rendered we can add a listener to Add to Cart button
-    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from
-    document.getElementById("addToCart")
-    .addEventListener("click", this.addToCart.bind(this));
+    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addToCart.bind(this));
   }
-
-  /* addToCart(){
-    const cartItems = getLocalStorage("so-cart") || [];
-    cartItems.push(this.product);
-    setLocalStorage("so-cart", this.product);
-  } */
-
   addToCart() {
-    // get the cart items from local storage
-    let cartItems = getLocalStorage("so-cart") || [];
-
-
-    // add the current product to the cart
-    cartItems.push(this.product);
-
-    // set the cart items in local storage
-    setLocalStorage("so-cart", cartItems);
-    document.dispatchEvent(new Event("cartUpdated"));
+    let cartContents = getLocalStorage("so-cart");
+    //check to see if there was anything there
+    if (!cartContents) {
+      cartContents = [];
+    }
+    // then add the current product to the list
+    cartContents.push(this.product);
+    setLocalStorage("so-cart", cartContents);
   }
-
   renderProductDetails(selector) {
-    const product = this.product;
     const element = document.querySelector(selector);
-    const template = `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
-    <h2 class="divider">${product.NameWithoutBrand}</h2>
-    <img
-        class="divider"
-        src="${product.Image}"
-        alt="${product.NameWithoutBrand}"
-    />
-    <p class="product-card__price">
-       <span class="product-card__discount">$${product.SuggestedRetailPrice} </span>
-       $${product.FinalPrice}
-       <span class="flag-discount">sale</span>
-    </p>
-    <p class="product__color">${product.Colors[0].ColorName}</p>
-    <p class="product__description">
-    ${product.DescriptionHtmlSimple}
-    </p>
-    <div class="product-detail__add">
-        <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-    </div></section>`;
-
-    element.insertAdjacentHTML("afterBegin", template);
-  };
+    element.insertAdjacentHTML(
+      "afterBegin",
+      productDetailsTemplate(this.product)
+    );
+  }
 }
-
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,15 +1,11 @@
-import ProductData from "./ProductData.mjs";
-import ProductListing from "./ProductList.mjs";
-//new code
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
+import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData("tents");
-const list = new ProductListing(
-  "Tents",
-  dataSource,
-  document.querySelector(".product-list")
-);
-
-list.init();
-//new code
 loadHeaderFooter();
+const category = getParam("category");
+const dataSource = new ExternalServices();
+const element = document.querySelector(".product-list");
+const listing = new ProductList(category, dataSource, element);
+
+listing.init();
